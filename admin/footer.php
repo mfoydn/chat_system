e4ed68b8bb70696fb2775c486d6b0d7d226192fb
Mfoydn <mfoydamasmfoy@gmail.com>
2018-06-15 09:01:36 +0300

    first commit

---
+++ admin/footer.php
@@ -0,0 +1,5 @@
+<footer class="footer footer-fixed-bottom" style="background-color: #00BCD4; text-align: center; height: 60px; width: 100%;  bottom: 0; position: absolute; padding-top: 20px;">
+      <div class="container">
+        <span class="text-muted">© <?php echo date('Y')?> Copyright: Tuzungumze</span>
+      </div>
+  </footer>
---
+++ chat_system.sql
@@ -0,0 +1,146 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 14, 2018 at 02:48 AM
+-- Server version: 10.1.32-MariaDB
+-- PHP Version: 5.6.36
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `chat_system`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chat`
+--
+
+CREATE TABLE `chat` (
+  `chatid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `chatroomid` int(11) NOT NULL,
+  `message` varchar(200) NOT NULL,
+  `chat_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chatroom`
+--
+
+CREATE TABLE `chatroom` (
+  `chatroomid` int(11) NOT NULL,
+  `chat_name` varchar(60) NOT NULL,
+  `date_created` datetime NOT NULL,
+  `chat_password` varchar(30) NOT NULL,
+  `userid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chat_member`
+--
+
+CREATE TABLE `chat_member` (
+  `chat_memberid` int(11) NOT NULL,
+  `chatroomid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `userid` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(150) NOT NULL,
+  `uname` varchar(60) NOT NULL,
+  `photo` varchar(200) NOT NULL,
+  `access` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`userid`, `username`, `password`, `uname`, `photo`, `access`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Admin', '', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `chat`
+--
+ALTER TABLE `chat`
+  ADD PRIMARY KEY (`chatid`);
+
+--
+-- Indexes for table `chatroom`
+--
+ALTER TABLE `chatroom`
+  ADD PRIMARY KEY (`chatroomid`);
+
+--
+-- Indexes for table `chat_member`
+--
+ALTER TABLE `chat_member`
+  ADD PRIMARY KEY (`chat_memberid`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`userid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `chat`
+--
+ALTER TABLE `chat`
+  MODIFY `chatid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `chatroom`
+--
+ALTER TABLE `chatroom`
+  MODIFY `chatroomid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `chat_member`
+--
+ALTER TABLE `chat_member`
+  MODIFY `chat_memberid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
